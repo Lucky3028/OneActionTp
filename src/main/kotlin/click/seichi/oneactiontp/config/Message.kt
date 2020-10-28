85fee549b21babcd1acd86be1e2aaea16a85e037
Lucky3028 <lucky3028.0@gmail.com>
2020-10-28 23:00:22 +0900

    [Update] Message.kt

--- src/main/kotlin/click/seichi/oneactiontp/config/Message.kt
+++ src/main/kotlin/click/seichi/oneactiontp/config/Message.kt
@@ -2,5 +2,9 @@ package click.seichi.oneactiontp.config
 
 object Message : Configs("message") {
 
-    val selfTeleport: String by lazy { getString("self-teleport") }
+    val playersOnlyCanExecute: String by lazy { getString("players_only_can_execute") }
+
+    val invalidArgs: String by lazy { getString("invalid_args") }
+
+    val selfTeleport: String by lazy { getString("self_teleport") }
 }
