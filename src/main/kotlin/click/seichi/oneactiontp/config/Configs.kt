6f3ade96e8f4eef8c31e4a85f4f0e50333a8b510
Lucky3028 <lucky3028.0@gmail.com>
2020-10-28 18:14:45 +0900

    [Add] Configs.kt

---
+++ src/main/kotlin/click/seichi/oneactiontp/config/Configs.kt
@@ -0,0 +1,17 @@
+package click.seichi.oneactiontp.config
+
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+
+abstract class Configs(private val ymlName: String): YamlConfiguration() {
+    fun a(plugin: JavaPlugin) {
+        val yml = File(plugin.dataFolder, "$ymlName.yml")
+        if (yml.exists()) this.load(yml)
+        else createNewFile(plugin, yml)
+    }
+
+    private fun createNewFile(plugin: JavaPlugin, yml: File) {
+        plugin.saveResource(yml.name, false)
+    }
+}
