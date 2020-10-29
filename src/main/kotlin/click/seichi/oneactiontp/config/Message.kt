32bda5187547edc4d535a8c5a138bcef991e09f6
Lucky3028 <lucky3028.0@gmail.com>
2020-10-29 21:01:00 +0900

    [Add] Message

--- src/main/kotlin/click/seichi/oneactiontp/config/Message.kt
+++ src/main/kotlin/click/seichi/oneactiontp/config/Message.kt
@@ -2,9 +2,29 @@ package click.seichi.oneactiontp.config
 
 object Message : Configs("message") {
 
-    val playersOnlyCanExecute: String by lazy { getString("players_only_can_execute") }
+    val playerOnlyExeCmd: String by lazy { getString("player_only_can_execute") }
 
     val invalidArgs: String by lazy { getString("invalid_args") }
 
     val selfTeleport: String by lazy { getString("self_teleport") }
+
+    val denyingAllReqs: String by lazy { getString("denying_all_req") }
+
+    val playerNotFound: String by lazy { getString("player_not_found") }
+
+    val hasSentReq: String by lazy { getString("has_sent_req") }
+
+    val clickToAccept: String by lazy { getString("click_to_accept") }
+
+    val clickToDeny: String by lazy { getString("click_to_deny") }
+
+    val hoverToAccept: String by lazy { getString("hover_to_accept") }
+
+    val hoverToDeny: String by lazy { getString("hover_to_deny") }
+
+    val reqHasArrived: List<String> by lazy { getStringList("req_has_arrived") }
+
+    val reqHasBeenSent: String by lazy { getString("req_has_been_sent") }
+
+    val reqHasExpired: String by lazy { getString("req_has_expired") }
 }
