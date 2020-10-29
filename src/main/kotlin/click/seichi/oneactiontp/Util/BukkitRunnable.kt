16eb8bf5a3baecd54e2880b4646d79c3e7be4ebc
Lucky3028 <lucky3028.0@gmail.com>
2020-10-29 14:04:34 +0900

    [Add] BukkitRunnableをまとめるUtil

---
+++ src/main/kotlin/click/seichi/oneactiontp/Util/BukkitRunnable.kt
@@ -0,0 +1,22 @@
+package click.seichi.oneactiontp.Util
+
+import click.seichi.oneactiontp.OneActionTp
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.scheduler.BukkitTask
+
+/**
+ * [BukkitRunnable.runTaskLaterAsynchronously]を短縮した関数。
+ *
+ * 非同期で待機時間をつけて処理を実行する。
+ *
+ * @param delay 待機時間（tick）
+ * @param task 実行する関数
+ * @return 指定された[BukkitRunnable.run]が設定された[BukkitTask]
+ */
+fun runTaskLaterAsynchronously(delay: Long, task: () -> Unit): BukkitTask {
+    return object : BukkitRunnable() {
+        override fun run() {
+            task()
+        }
+    }.runTaskLaterAsynchronously(OneActionTp.PLUGIN, delay)
+}
