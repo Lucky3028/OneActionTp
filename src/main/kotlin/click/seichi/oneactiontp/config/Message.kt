94739190a581eeba6e79687c780bab0ef91499ec
Lucky3028 <lucky3028.0@gmail.com>
2020-11-16 22:59:21 +0900

    [Fix] messageをカラーコード指定に対応する

--- src/main/kotlin/click/seichi/oneactiontp/config/Message.kt
+++ src/main/kotlin/click/seichi/oneactiontp/config/Message.kt
@@ -1,36 +1,38 @@
 package click.seichi.oneactiontp.config
 
+import click.seichi.oneactiontp.util.convertColorCode
+
 object Message : Configs("message") {
 
-    val playerOnlyExeCmd: String by lazy { getString("player_only_can_execute") }
+    val playerOnlyExeCmd: String by lazy { convertColorCode(getString("player_only_can_execute")) }
 
-    val invalidArgs: String by lazy { getString("invalid_args") }
+    val invalidArgs: String by lazy { convertColorCode(getString("invalid_args")) }
 
-    val selfTeleport: String by lazy { getString("self_teleport") }
+    val selfTeleport: String by lazy { convertColorCode(getString("self_teleport")) }
 
-    val denyingAllReqs: String by lazy { getString("denying_all_req") }
+    val denyingAllReqs: String by lazy { convertColorCode(getString("denying_all_req")) }
 
-    val playerNotFound: String by lazy { getString("player_not_found") }
+    val playerNotFound: String by lazy { convertColorCode(getString("player_not_found")) }
 
-    val hasSentReq: String by lazy { getString("has_sent_req") }
+    val hasSentReq: String by lazy { convertColorCode(getString("has_sent_req")) }
 
-    val clickToAccept: String by lazy { getString("click_to_accept") }
+    val clickToAccept: String by lazy { convertColorCode(getString("click_to_accept")) }
 
-    val clickToDeny: String by lazy { getString("click_to_deny") }
+    val clickToDeny: String by lazy { convertColorCode(getString("click_to_deny")) }
 
-    val hoverToAccept: String by lazy { getString("hover_to_accept") }
+    val hoverToAccept: String by lazy { convertColorCode(getString("hover_to_accept")) }
 
-    val hoverToDeny: String by lazy { getString("hover_to_deny") }
+    val hoverToDeny: String by lazy { convertColorCode(getString("hover_to_deny")) }
 
-    val reqHasArrived: List<String> by lazy { getStringList("req_has_arrived") }
+    val reqHasArrived: List<String> by lazy { getStringList("req_has_arrived").map { convertColorCode(it) } }
 
-    val reqHasBeenSent: String by lazy { getString("req_has_been_sent") }
+    val reqHasBeenSent: String by lazy { convertColorCode(getString("req_has_been_sent")) }
 
-    val reqHasExpired: String by lazy { getString("req_has_expired") }
+    val reqHasExpired: String by lazy { convertColorCode(getString("req_has_expired")) }
 
-    val noPendingTppReq: String by lazy { getString("no_pending_tpp_req") }
+    val noPendingTppReq: String by lazy { convertColorCode(getString("no_pending_tpp_req")) }
 
-    val senderDeniedTppReq: String by lazy { getString("sender_denied_tpp_req") }
+    val senderDeniedTppReq: String by lazy { convertColorCode(getString("sender_denied_tpp_req")) }
 
-    val receiverDenyTppReq: String by lazy { getString("receiver_deny_tpp_req") }
+    val receiverDenyTppReq: String by lazy { convertColorCode(getString("receiver_deny_tpp_req")) }
 }
--- src/main/kotlin/click/seichi/oneactiontp/util/MsgSender.kt
+++ src/main/kotlin/click/seichi/oneactiontp/util/MsgSender.kt
@@ -1,7 +1,12 @@
 package click.seichi.oneactiontp.util
 
+import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.chat.BaseComponent
 import org.bukkit.command.CommandSender
+
+fun CommandSender.sendMsg(msg: String) = this.sendMessage(convertColorCode(msg))
+
+fun CommandSender.sendMsgs(vararg msgs: String) = msgs.forEach { this.sendMessage(convertColorCode(it)) }
 
 /**
  * [CommandSender.Spigot.sendMessage]を省略して利用できる関数。
@@ -12,3 +17,5 @@ fun CommandSender.sendMsg(msg: BaseComponent) = this.spigot().sendMessage(msg)
  * [CommandSender.sendMsg]の引数に配列を指定できる関数。
  */
 fun CommandSender.sendMsgs(vararg msgs: BaseComponent) = msgs.forEach { this.spigot().sendMessage(it) }
+
+fun convertColorCode(msg: String): String = ChatColor.translateAlternateColorCodes('&', msg)
