f97333ef6906062f2c1190e40968ff7d827171a3
Lucky3028 <lucky3028.0@gmail.com>
2020-11-24 15:15:26 +0900

    Add TODO

--- src/main/kotlin/click/seichi/oneactiontp/cpllection/RequestsLimitation.kt
+++ src/main/kotlin/click/seichi/oneactiontp/cpllection/RequestsLimitation.kt
@@ -14,6 +14,7 @@ object RequestsLimitation {
 
         flagMap[player] = new
 
+        // TODO Messageにする
         if (new) {
             player.sendMessage("${ChatColor.RED}OneActionTPのプレイヤー間テレポート申請をすべて拒否します。")
         } else {
